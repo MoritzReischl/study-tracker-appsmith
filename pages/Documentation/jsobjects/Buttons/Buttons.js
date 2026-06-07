2897ded65a4754b920d9c86e67d485a349e61d54
doc.sys.main.2025 <doc.sys.main.2025@gmail.com>
2026-06-07 11:08:58 +0000

    Updated history upon insertion

--- pages/Documentation/jsobjects/Buttons/Buttons.js
+++ pages/Documentation/jsobjects/Buttons/Buttons.js
@@ -1,68 +1,74 @@
 export default {
-	async delete() {
-		try{	
-			await Auth.refreshTokens();
-			await DeleteRow.run({id:appsmith.store.delete_id});
-			await SelectAll.run();
-			closeModal('delete_modal');
-			showAlert("The row was deleted.","warning");
-		} catch(e){
-			showAlert([
-				e?.message,
-				DeleteRow.data?.message,
-				DeleteRow.data?.details,
-				DeleteRow.data?" (Code "+DeleteRow.data.code+")":null]
-				.filter(Boolean)
-				.join(". "),
-				"error")
-		};
-	},
-	
-	async insert(){
-		try{
-			await Auth.refreshTokens();
-			await InsertRow.run();
-			await SelectAll.run();
-			showAlert("Row has been inserted successfully.","success");
-		} catch(e) {
-			showAlert([
-				e?.message,
-				InsertRow.data?.message,
-				InsertRow.data?.details,
-				InsertRow.data?" (Code "+InsertRow.data.code+")":null]
-				.filter(Boolean)
-				.join(". "),
-				"error")
-		};
-	},
+  async delete() {
+    try {	
+      await Auth.callApi(DeleteRow, { id: appsmith.store.delete_id });
+      await Auth.callApi(SelectAll);
 
-	async getUpdateDefaults(){
-		await Auth.callApi(SelectUpdateDefault, {
-			project_id: String(appsmith.store.UpdateDefaultId)
-		});
-	},
+      closeModal('delete_modal');
+      showAlert("The row was deleted.", "warning");
+    } catch(e) {
+      showAlert([
+        e?.message,
+        DeleteRow.data?.message,
+        DeleteRow.data?.details,
+        DeleteRow.data ? " (Code " + DeleteRow.data.code + ")" : null
+      ]
+        .filter(Boolean)
+        .join(". "),
+        "error"
+      );
+    }
+  },
 	
-	async update(){
-		try{
-			await Auth.refreshTokens();
-			await UpdateRow.run();
-			SelectAll.run();
-			closeModal("update_modal");
-			showAlert("Row has been updated successfully.","success")
-		} catch(e) {
-			showAlert([
-				e?.message,
-				UpdateRow.data?.message,
-				UpdateRow.data?.details,
-				UpdateRow.data?" (Code "+UpdateRow.data.code+")":null]
-				.filter(Boolean)
-				.join(". \r\n"),
-				"error")
-		}
-	},
+  async insert() {
+    try {
+      await Auth.runAuthenticated(() => InsertRow.run());
+      await Auth.callApi(SelectAll);
+
+      showAlert("Row has been inserted successfully.", "success");
+    } catch(e) {
+      showAlert([
+        e?.message,
+        InsertRow.data?.message,
+        InsertRow.data?.details,
+        InsertRow.data ? " (Code " + InsertRow.data.code + ")" : null
+      ]
+        .filter(Boolean)
+        .join(". "),
+        "error"
+      );
+    }
+  },
+
+  async getUpdateDefaults() {
+    await Auth.callApi(SelectUpdateDefault, {
+      project_id: String(appsmith.store.UpdateDefaultId)
+    });
+  },
 	
-	async refreshTableAndButtons(){
-		await SelectAll.run();
-		Fields_and_Display.loadProjects();
-	}
+  async update() {
+    try {
+      await Auth.runAuthenticated(() => UpdateRow.run());
+      await Auth.callApi(SelectAll);
+
+      closeModal("update_modal");
+      showAlert("Row has been updated successfully.", "success");
+    } catch(e) {
+      showAlert([
+        e?.message,
+        UpdateRow.data?.message,
+        UpdateRow.data?.details,
+        UpdateRow.data ? " (Code " + UpdateRow.data.code + ")" : null
+      ]
+        .filter(Boolean)
+        .join(". \r\n"),
+        "error"
+      );
+    }
+  },
+	
+  async refreshTableAndButtons() {
+    await Auth.callApi(SelectAll);
+    await Fields_and_Display.loadProjects();
+  }
 }
